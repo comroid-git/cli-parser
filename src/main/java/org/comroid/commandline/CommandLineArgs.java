b8f12f968693b77bbc8b106fa69f217a922b0d49
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-10-10 17:19:01 +0200

    code cleanup

--- src/main/java/org/comroid/commandline/CommandLineArgs.java
+++ src/main/java/org/comroid/commandline/CommandLineArgs.java
@@ -1,12 +1,9 @@
 package org.comroid.commandline;
 
 import org.comroid.mutatio.ref.KeyedReference;
-import org.comroid.mutatio.ref.Reference;
-import org.comroid.mutatio.ref.ReferenceIndex;
 import org.comroid.mutatio.ref.ReferenceMap;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public final class CommandLineArgs extends ReferenceMap.Support.Basic<String, String> {
     private CommandLineArgs(Map<String, KeyedReference<String, String>> values) {
